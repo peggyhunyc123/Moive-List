8bdb9d1c48a2ddd3fd625b2ee96b763d20266975
peggyhunyc123 <47424725+peggyhunyc123@users.noreply.github.com>
2019-03-29 17:45:06 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import APP from './APP';
+import App from './App';
 
 
-ReactDOM.render(<APP />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
