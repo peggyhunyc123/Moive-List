65d5874c8bf2ceb1b264c42fd79c67e1b71a37ee
Peggy <peggyhunyc123@gmail.com>
2019-03-31 23:10:42 -0400

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,12 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import reducer from './reducer';
+import thunk from 'redux-thunk';
+
 
 const store = createStore(
     reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(thunk),
+    //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 export default store;
