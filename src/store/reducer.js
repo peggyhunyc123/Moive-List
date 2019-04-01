4fa4b24c68c92e4e59629f37690ae8c067f32192
Peggy <peggyhunyc123@gmail.com>
2019-03-31 23:11:14 -0400

    Update reducer.js

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,17 +1,22 @@
-import data from '../data.json';
+
 
 const defaultState = {
     show: 1000000,
-    mylist: data.mylist,
-    recommendations: data.recommendations
+    mylist: [],
+    recommendations: []
 }
 
 export default (state = defaultState, action) => {
+    if (action.type === 'GET_DATA') {
+        const newState = JSON.parse(JSON.stringify(state));
+        newState.mylist = action.data.mylist;
+        newState.recommendations = action.data.recommendations;
+        return newState;
+    }
     if (action.type === 'ADD_MOVIE') {
         const newState = JSON.parse(JSON.stringify(state));
         newState.mylist.push(newState.recommendations[action.index]);
         newState.recommendations.splice(action.index, 1);
-
         return newState;
     }
     if (action.type === 'REMOVE_MOVIE') {
@@ -22,7 +27,6 @@ export default (state = defaultState, action) => {
     }
     if (action.type === 'MOUSE_OVER') {
         const newState = JSON.parse(JSON.stringify(state));
-        
         newState.show = action.id;
         return newState;
     }
